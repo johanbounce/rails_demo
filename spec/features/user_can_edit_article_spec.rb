bbab8973309f4f039854fb3764b7201f05815657
bounce <johan@waytogo.se>
2020-02-17 06:58:07 +0100

    Creating tests for edit article, all passed

--- spec/features/user_can_edit_article_spec.rb
+++ spec/features/user_can_edit_article_spec.rb
@@ -1,7 +1,7 @@
 feature 'User can' do
     context 'edit and article' do
         before do
-            create(:article, title: 'Hey errady', content: 'Play pookers')
+            create(:article, title: 'Hello errady', content: 'Play pookers')
             visit root_path
             click_on 'Edit'
         end
@@ -14,6 +14,12 @@ feature 'User can' do
             fill_in 'Content', with: 'Best citys for purties'
             click_on 'Update Article'
             expect(page).to have_content 'Best citys for purties'
+        end
+
+        it 'User can edit title' do
+            fill_in 'Title', with: 'Vegas Baby'
+            click_on 'Update Article'
+            expect(page).to have_content 'Vegas Baby'
         end
     end
 end
